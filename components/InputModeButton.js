185177227b2728f7cf9b2cdc795ac4797a1b4996
Suhan Nath <snath6@uic.edu>
2020-11-28 12:15:01 -0600

    :art: Updating Avatar SVG

--- components/InputModeButton.js
+++ components/InputModeButton.js
@@ -1,36 +1,49 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Button,Icon } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Button,
+  Icon,
+} from 'react-native';
 import { Foundation } from '@expo/vector-icons';
 import MyAppText from '../components/MyAppText';
+import { useTheme } from '@react-navigation/native';
 
-const InputModeButton = ({icon, action}) => {
-    return (
-       <TouchableOpacity style={styles.button} onPress={() => action()}>
-       <Foundation
-            style={[styles.icon]}
-            name={icon}
-            size={60}
-            color="#2A9D8F"
-          />
-      </TouchableOpacity>
-    );
+const InputModeButton = ({ icon, action }) => {
+  const { tertiaryColor, secondaryColor } = useTheme();
+  return (
+    <TouchableOpacity
+      style={styles({ tertiaryColor }).button}
+      onPress={() => action()}
+    >
+      <Foundation
+        style={[styles.icon]}
+        name={icon}
+        size={45}
+        color={secondaryColor}
+      />
+    </TouchableOpacity>
+  );
 };
 
-const styles = StyleSheet.create({
+const styles = ({ tertiaryColor }) =>
+  StyleSheet.create({
     icon: {
-        marginTop: 20,
-        marginHorizontal: 20,
+      marginTop: 20,
+      marginHorizontal: 20,
     },
-    button: { 
-        borderWidth:1,
-        borderColor:'rgba(0,0,0,0.2)',
-        alignItems:'center',
-        justifyContent:'center',
-        width:120,
-        height:120,
-        backgroundColor:'#fff',
-        borderRadius:100,
-    }
-});
+    button: {
+      borderWidth: 1,
+      borderColor: 'rgba(0,0,0,0.2)',
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: 100,
+      height: 100,
+      backgroundColor: tertiaryColor,
+      borderRadius: 100,
+    },
+  });
 
 export default InputModeButton;
--- components/MessageBubble.js
+++ components/MessageBubble.js
@@ -1,151 +1,136 @@
 // Import react
-import React from 'react'
+import React from 'react';
 
 // Import react-native components
-import {
-  StyleSheet,
-  View,
-  Text,
-  Image
-} from 'react-native'
+import { StyleSheet, View, Text, Image } from 'react-native';
+
+import { useTheme } from '@react-navigation/native';
 
 // Import react-native-svg
 // from 'https://github.com/react-native-community/react-native-svg'
-import Svg, { Path } from 'react-native-svg'
+import Svg, { Path } from 'react-native-svg';
 
 // Import react-native-size-matters
 // from 'https://github.com/nirsky/react-native-size-matters'
-import { moderateScale } from 'react-native-size-matters'
+import { moderateScale } from 'react-native-size-matters';
 
 // Props info list
 // 1. mine (bool) => renders blue bubble on right
 // 2. text (string) => renders text message
 // 3. image (image file) => renders image inside bubble
 
-// Declare component 
-class MessageBubble extends React.Component {
-  render(){
-    return (
-      <View style={[
-          styles.message,
-          this.props.mine ? styles.mine : styles.not_mine
+// Declare component
+const MessageBubble = (props) => {
+  const { tertiaryColor, secondaryColor, accentColor } = useTheme();
+
+  return (
+    <View style={[styles.message, props.mine ? styles.mine : styles.not_mine]}>
+      <View
+        style={[
+          styles.cloud,
+          {
+            backgroundColor: props.mine ? tertiaryColor : accentColor,
+          },
         ]}
       >
-        <View
-          style={[
-            styles.cloud,
-            {
-              backgroundColor: this.props.mine ? '#dddddd' : '#007aff'
-            }
-          ]}
-        >
-          {
-            this.props.image
-            ?
-              <Image
-                style={{alignSelf: this.props.mine ? 'flex-start' : 'flex-end' }}
-                borderRadius={10}
-                source={this.props.image}
-              />
-            :
-              null
-          }
-          {
-            this.props.text 
-            ?
-              <Text
-                style={[
-                  styles.text,
-                  {
-                    color: this.props.mine ? 'black' : 'white'
-                  }
-                ]}
-              >
-                {this.props.text}
-              </Text>
-            :
-              null
-          }
-          <View
+        {props.image ? (
+          <Image
+            style={{ alignSelf: props.mine ? 'flex-start' : 'flex-end' }}
+            borderRadius={10}
+            source={props.image}
+          />
+        ) : null}
+        {props.text ? (
+          <Text
             style={[
-              styles.arrow_container,
-              this.props.mine ? styles.arrow_left_container : styles.arrow_right_container
+              styles.text,
+              {
+                color: secondaryColor,
+              },
             ]}
           >
-            <Svg
-              style={this.props.mine ? styles.arrow_left : styles.arrow_right}
-              width={moderateScale(15.5, 0.6)}
-              height={moderateScale(17.5, 0.6)}
-              viewBox="32.484 17.5 15.515 17.5"
-              enable-background="new 32.485 17.5 15.515 17.5"
-            >
-              <Path
-                  d={ this.props.mine 
-                      ? 
-                      "M38.484,17.5c0,8.75,1,13.5-6,17.5C51.484,35,52.484,17.5,38.484,17.5z"
-                      : 
-                      "M48,35c-7-4-6-8.75-6-17.5C28,17.5,29,35,48,35z"
-                  }
-                  fill={this.props.mine ? '#dddddd' : '#007AFF'}
-                  x="0"
-                  y="0"
-              />
-            </Svg>
-          </View>
+            {props.text}
+          </Text>
+        ) : null}
+        <View
+          style={[
+            styles.arrow_container,
+            props.mine
+              ? styles.arrow_left_container
+              : styles.arrow_right_container,
+          ]}
+        >
+          <Svg
+            style={props.mine ? styles.arrow_left : styles.arrow_right}
+            width={moderateScale(15.5, 0.6)}
+            height={moderateScale(17.5, 0.6)}
+            viewBox="32.484 17.5 15.515 17.5"
+            enable-background="new 32.485 17.5 15.515 17.5"
+          >
+            <Path
+              d={
+                props.mine
+                  ? 'M38.484,17.5c0,8.75,1,13.5-6,17.5C51.484,35,52.484,17.5,38.484,17.5z'
+                  : 'M48,35c-7-4-6-8.75-6-17.5C28,17.5,29,35,48,35z'
+              }
+              fill={props.mine ? tertiaryColor : accentColor}
+              x="0"
+              y="0"
+            />
+          </Svg>
         </View>
       </View>
-    )
-  }
-}
+    </View>
+  );
+};
 
-export default MessageBubble
+export default MessageBubble;
 
 const styles = StyleSheet.create({
   message: {
     flexDirection: 'row',
-    marginVertical: moderateScale(7,2)
+    marginVertical: moderateScale(7, 2),
   },
   mine: {
     marginLeft: 20,
   },
   not_mine: {
     alignSelf: 'flex-end',
-    marginRight: 20
+    marginRight: 20,
   },
   cloud: {
-    maxWidth: moderateScale(250,2),
-    paddingHorizontal: moderateScale(10,2),
-    paddingTop: moderateScale(5,2),
-    paddingBottom: moderateScale(7,2),
-    borderRadius: 20
+    maxWidth: moderateScale(250, 2),
+    paddingHorizontal: moderateScale(10, 2),
+    paddingTop: moderateScale(5, 2),
+    paddingBottom: moderateScale(7, 2),
+    borderRadius: 20,
   },
   text: {
     paddingTop: 3,
     fontSize: 17,
-    lineHeight: 22
+    lineHeight: 22,
   },
   arrow_container: {
-    position:'absolute',
+    position: 'absolute',
     top: 0,
-    left:0,
+    left: 0,
     right: 0,
     bottom: 0,
     zIndex: -1,
-    flex: 1
+    flex: 1,
   },
   arrow_left_container: {
     justifyContent: 'flex-end',
-    alignItems: 'flex-start'
+    alignItems: 'flex-start',
   },
   arrow_right_container: {
     justifyContent: 'flex-end',
-    alignItems: 'flex-end'
+    alignItems: 'flex-end',
   },
   arrow_left: {
-    left: moderateScale(-6, 0.5)
+    left: moderateScale(-6, 0.5),
   },
   arrow_right: {
-    right: moderateScale(-6, 0.5)
-  }
-
-})
+    right: moderateScale(-6, 0.5),
+  },
+});

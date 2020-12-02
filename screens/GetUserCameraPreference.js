1fa3232c02b6f09cf6219a8799f4c485fa565523
Suhan Nath <snath6@uic.edu>
2020-12-02 02:28:32 -0600

    :art: CSS changes for Modal and Camera Screens

--- screens/GetUserCameraPreference.js
+++ screens/GetUserCameraPreference.js
@@ -1,102 +1,114 @@
-import React, {useState,useEffect } from 'react';
-import { StyleSheet, Text, View, Button, Modal, TouchableHighlight } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Modal,
+  TouchableOpacity,
+} from 'react-native';
 import ConversationCard from '../components/ConversationCard';
 import { Ionicons } from '@expo/vector-icons';
-import TaskCreatedModal from "../components/TaskCreatedModal";
-import { CommonActions } from "@react-navigation/native";
-import {markTaskAsDone} from "../database/Utilities/api"
+import MyAppText from '../components/MyAppText';
+import TaskCreatedModal from '../components/TaskCreatedModal';
+import { CommonActions, useTheme } from '@react-navigation/native';
+import { markTaskAsDone } from '../database/Utilities/api';
 
+const GetUserCameraPreference = ({ route, navigation }) => {
+  const [isVisible, setIsVisible] = useState(false);
 
-const GetUserCameraPreference = ({ route,navigation }) => {
-    
-    const [isVisible, setIsVisible] = useState(false);
+  const taskID = route.params.taskId;
 
-    const taskID = route.params.taskId;
+  const markAsDone = async () => {
+    await markTaskAsDone(taskID);
+  };
 
-    const markAsDone = async () => {
-        await markTaskAsDone(taskID);
-    }
-
-    const navigate = () => {
-        navigation.dispatch(
-            CommonActions.reset({
-              index: 1,
-              routes: [
-                { name: 'Home', params: { headerShown: false } },
-                {
-                  name: 'ViewTasks',
-                  params: { title: 'View Tasks', headerShown: false },
-                },
-              ],
-            }),
-          );
-    }
-
- 
-    return (
-        <View style={styles.container}>
-           {TaskCreatedModal(isVisible, navigate,["You're all set", "I have created the task for you"])}
-            <View style={{flex:4}}>
-
-            <ConversationCard avatarText="Do you want to add a picture to the task before marking it as done?"/>
-            </View>
-            <View style={{flex:2,flexDirection: 'column', alignSelf:'center', width:200}}>
-                <View style={[{  margin:10, flexDirection:'row', width:'100%', justifyContent:'center'  }]}>
-                    <Button
-                        title="Yes"
-                        color="#000"
-                        backgroundColor='#FFF'
-                        onPress = {()=> navigation.navigate('TakePhotoFromCamera',{taskID:taskID})}
-                    />
-                    </View>
-                    <View style={[{ width:200, flexDirection:'row', margin:10, justifyContent:'center'}]}>
-                    <Button
-                        onPress = {async () => {
-                            await markAsDone();
-                            setIsVisible(!isVisible)}
-                        }
-                        title="No "
-                        color="#000"
-                        backgroundColor='#FFF'
-                    />
-                    </View>
-              </View>
-        </View>
+  const navigate = () => {
+    navigation.dispatch(
+      CommonActions.reset({
+        index: 1,
+        routes: [
+          { name: 'Home', params: { headerShown: false } },
+          {
+            name: 'ViewTasks',
+            params: { title: 'View Tasks', headerShown: false },
+          },
+        ],
+      }),
     );
+  };
+
+  const option = (text, action) => {
+    const { tertiaryColor } = useTheme();
+
+    return (
+      <TouchableOpacity
+        style={styles({ tertiaryColor }).button}
+        onPress={() => action()}
+      >
+        <MyAppText>
+          <Text style={styles({}).modeText}>{text}</Text>
+        </MyAppText>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <View style={styles({}).container}>
+      {TaskCreatedModal(isVisible, navigate, [
+        "You're all set",
+        'I have created the task for you',
+      ])}
+      <View style={{ flex: 6 }}>
+        <ConversationCard avatarText="Do you want to add a picture to the task before marking it as done?" />
+      </View>
+      <View style={{ flex: 3, flexDirection: 'row' }}>
+        <View style={styles({}).options}>
+          {option('Yes', () => {
+            navigation.navigate('TakePhotoFromCamera', { taskID: taskID });
+          })}
+          {option('No', async () => {
+            //await markAsDone();
+            setIsVisible(!isVisible);
+          })}
+        </View>
+      </View>
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({
+const styles = ({ tertiaryColor }) =>
+  StyleSheet.create({
     icon: {
-        marginTop: 20,
-        marginHorizontal: 20,
+      marginTop: 20,
+      marginHorizontal: 20,
     },
     container: {
       flex: 1,
-      
+
       justifyContent: 'center',
       backgroundColor: '#264653',
-      flexDirection:'column'
+      flexDirection: 'column',
     },
-    button: { 
-        borderWidth:1,
-        borderColor:'rgba(0,0,0,0.2)',
-        alignItems:'center',
-        justifyContent:'center',
-        width:120,
-        height:120,
-        backgroundColor:'#fff',
-        borderRadius:100,
+    button: {
+      borderWidth: 1,
+      borderColor: 'rgba(0,0,0,0.2)',
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: 120,
+      height: 120,
+      backgroundColor: tertiaryColor,
+      borderRadius: 100,
     },
     options: {
-        flex: 6,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-around',
-    }
-});
-
-
-
-
+      flex: 6,
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-around',
+    },
+    modeText: {
+      fontSize: 35,
+    },
+  });
 
 export default GetUserCameraPreference;
